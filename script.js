2042876a9af28583bdd0988792e27fc457ee84db
Srishh29 <srishtidiwani07@gmail.com>
2026-06-22 17:08:32 +0530

    add theme feature and fix bugs

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let logo = document.querySelector(".logo");
 
+// Image sources for the two logos
+
 const img1 = "bnd.png";
 const img2 = "bnd2.png";
 
@@ -16,4 +18,14 @@ logo.addEventListener("mouseenter", () => {
 logo.addEventListener("mouseleave", () => {
     clearInterval(intervalId);
     showingFirst = true;
+});
+
+//theme toggle
+let themeIcon = document.querySelector(".theme");
+let isdark = true;
+
+themeIcon.addEventListener("click", () => {
+    themeIcon.src = isdark ? "lighttheme.png" : "darktheme.png";
+    document.body.style.backgroundColor = isdark ? "#c79a66" : "#5f3723";
+    isdark = !isdark;
 });
