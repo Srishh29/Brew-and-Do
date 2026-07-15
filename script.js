55e721189c4b664e24dec9a68c1d50a2b1cd58f0
Srishh29 <srishtidiwani07@gmail.com>
2026-07-15 21:23:29 +0530

    new features

--- script.js
+++ script.js
@@ -5,20 +5,20 @@ let logo = document.querySelector(".logo");
 const img1 = "bnd.png";
 const img2 = "bnd2.png";
 
-let intervalId;
-let showingFirst = true;
+// let intervalId;
+// let showingFirst = true;
 
-logo.addEventListener("mouseenter", () => {
-    intervalId = setInterval(() => {
-        logo.src = showingFirst ? img2 : img1;
-        showingFirst = !showingFirst;
-    }, 400); // toggle every 500 ms
-});
+// logo.addEventListener("mouseenter", () => {
+//     intervalId = setInterval(() => {
+//         logo.src = showingFirst ? img2 : img1;
+//         showingFirst = !showingFirst;
+//     }, 400); // toggle every 500 ms
+// });
 
-logo.addEventListener("mouseleave", () => {
-    clearInterval(intervalId);
-    showingFirst = true;
-});
+// logo.addEventListener("mouseleave", () => {
+//     clearInterval(intervalId);
+//     showingFirst = true;
+// });
 
 //theme toggle
 let themeIcon = document.querySelector(".theme");
